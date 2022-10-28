85863e7ab47b5898a55d4f4c872381f7252577ce
crnndavid <95691870+crnndavid@users.noreply.github.com>
2022-10-28 10:33:42 +0200

    Add Global Stylecheets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import Logo from "../src/assets/img/logo-agence-montmartre.svg";
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <img src={Logo} alt="" />
     </div>
   );
 }
